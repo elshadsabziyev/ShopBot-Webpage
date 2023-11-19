6a42a8a0c20ecaae087c52d011ccd6ef97d5461d
elshadsabziyev <79898558+elshadsabziyev@users.noreply.github.com>
2023-11-19 04:07:27 +0400

    Fix bug in login functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,11 +8,12 @@ import Head from "next/head";
 const Navbar = async () => {
   return (
     <div className="border-b">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        
-      </Head>
       <Container>
+        <Head>
+          <title>ShopBot</title>
+        </Head>
+        //favicon
+        <link rel="icon" href="/favicon.ico" />
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">ShopBot</p>
